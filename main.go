59733281fbaf97c40796fb4f42a34c4da31e5304
ThunderUSK <daria_usk@mail.ru>
2021-01-26 17:41:26 +0300

    Go, 4-th lesson

--- main.go
+++ main.go
@@ -5,23 +5,24 @@ import ("net/http")
 import ("html/template")
 
 type User struct {
-	name string 
-	age uint16
-	money int16
-	avg_grades, happiness float64
+	Name string 
+	Age uint16
+	Money int16
+	Avg_grades, Happiness float64
+	Hobbies []string 
 }
 
 func (u User) getAllInfo() string {
 	return fmt.Sprintf("User name is: %s. Age of %d. Her" + 
-	" money: %d", u.name, u.age, u.money)
+	" money: %d", u.Name, u.Age, u.Money)
 }
 
 func (u *User) setNewName(newName string) {
-	u.name = newName
+	u.Name = newName
 } 
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-	mari := User{"Mari", 20, 250, 3.8, 0.95 }
+	mari := User{"Mari", 20, 25, 3.8, 0.9, []string{"Languages", "Books", "Travels"}}
 	// fmt.Fprintf(w, "<b>Main Text </b>")
 	tmpl, _ := template.ParseFiles("templates/home_page.html")
 	tmpl.Execute(w, mari)
